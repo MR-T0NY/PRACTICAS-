9916e7ed6d274800381c7bcd9dcafd04501d0a17
MR-T0NY <antonio.bandac@uanl.edu.mx>
2022-10-10 18:58:19 -0500

    Se añade codigo fuente

---
+++ Practica 1/src/calculadora/IdisOperaciones_Avan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author banda
+ */
+public interface IdisOperaciones_Avan {
+ 
+    public double Elevado(double a);
+     
+      public double Raiz(double a);
+  
+}
---
+++ Practica 1/src/calculadora/OperacionesAvanz.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author banda
+ */
+public class OperacionesAvanz {
+    
+     public double Elevado(double a){
+      return a*a;    
+    }
+     
+      public double Raiz(double a){
+      return Math.sqrt(a);
+}
+}
